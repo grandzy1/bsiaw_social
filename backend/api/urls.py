de39db9520cad7528fa3249ea238da5a6357e9f0
Kacper Jeziorski <kacper.jeziorski96@gmail.com>
2025-11-04 23:25:28 +0100

    dev v1.5

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = "api"
 urlpatterns = [
-        path("/users/register/",...,...),
+        path("users/register/", views.register_user, name="register_user"),
         path("/users/login/",...,...),
-         path('posts/', views.post_list, name='post-list'),
+        path('posts/', views.post_list, name='post-list'),
         path("/posts/create/<int:id>",...,...),
         path("/posts/edit/<int:id>",...,...),
         

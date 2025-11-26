d44d4049c3ca4f8e0d4ca779d9ea43d6ee9d855c
Kacper Jeziorski <kacper.jeziorski96@gmail.com>
2025-11-26 23:37:45 +0100

    change Terraform ecs.tf images (temporary)

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -101,7 +101,8 @@ resource "aws_ecs_task_definition" "backend" {
 
   container_definitions = jsonencode([{
     name      = "backend"
-    image     = "${aws_ecr_repository.backend.repository_url}:latest" 
+    # image     = "${aws_ecr_repository.backend.repository_url}:latest" 
+    image = "875707075546.dkr.ecr.us-east-1.amazonaws.com/webapp/backend" # temporary do sprawdzenia działania terraforma
     essential = true
     portMappings = [{
       containerPort = 8000
@@ -147,7 +148,8 @@ resource "aws_ecs_task_definition" "frontend" {
 
   container_definitions = jsonencode([{
     name      = "frontend"
-    image     = "${aws_ecr_repository.frontend.repository_url}:latest"
+    # image     = "${aws_ecr_repository.frontend.repository_url}:latest"
+    image = "875707075546.dkr.ecr.us-east-1.amazonaws.com/webapp/frontend" # temporary do sprawdzenia działania terraforma
     essential = true
     portMappings = [{
       containerPort = 3000

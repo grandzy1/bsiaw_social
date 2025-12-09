32639c91257dfef6aafd738d999896b87bb643bc
Sadowski-Krystian <krystek.sadowski@gmail.com>
2025-12-09 19:17:29 +0100

    Add headers

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Zastosuj te nagłówki do wszystkich ścieżek w aplikacji
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            // Poniżej znajduje się wartość polityki (jedna długa linia)
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:;",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff', // <--- TO JEST NOWA LINIJKA
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
